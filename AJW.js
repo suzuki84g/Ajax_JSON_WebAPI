4cc19b6999cd0cd453d87e64f757323f4bc5f0a8
suzuki84g <suzuki.84g@gmail.com>
2020-04-28 14:54:37 +0900

    buttonの動作確認

--- AJW.js
+++ AJW.js
@@ -1,22 +1,23 @@
-const button = document.getElementById('btn')
+const push = document.getElementById('btn');
 
 // ボタン押下時の動作
-button.onclick = () => {
+push.onclick = () => {
     // script要素を生成する
-        // script要素のsrc属性にリクエストURLを指定する。
-        // リクエストURL→ ' ベースとなるURL  ? ' +  Zipcodeに入力された郵便番号を指定  + '& callbackに関数名を指定する  ';
+    // script要素のsrc属性にリクエストURLを指定する。
+    // リクエストURL→ ' ベースとなるURL  ? ' +  Zipcodeに入力された郵便番号を指定  + '& callbackに関数名を指定する  ';
 
     // body要素の末尾にscript要素を追加する。
     // body要素のscript要素を削除する。
-}
+    alert('testです');
+};
 
 // 戻ってきた値の処理
-`callbackに指定した関数名`(data) {
-    if (data.results) {
-    // 都道府県の欄に都道府県名を表示する。
-    // 市区町村の欄に市区町村名を表示する。
-    // 住所の欄に町域名を表示する。
-    } else {
-        alert('該当するデータが見つかりませんでした');
-    }
-}
+// callback(data) {
+//     if (data.results) {
+//     // 都道府県の欄に都道府県名を表示する。
+//     // 市区町村の欄に市区町村名を表示する。
+//     // 住所の欄に町域名を表示する。
+//     } else {
+//         alert('該当するデータが見つかりませんでした');
+//     };
+// };
