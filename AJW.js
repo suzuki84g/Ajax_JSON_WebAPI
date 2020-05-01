a7cbe90df1f97a2881df0672e66a3811b67a07ff
suzuki84g <suzuki.84g@gmail.com>
2020-05-01 11:39:45 +0900

    JSONデータの読み込み完了

--- AJW.js
+++ AJW.js
@@ -5,7 +5,7 @@ push.onclick = () => {
     // script要素を生成する
     let newScript = document.createElement('script');  // scriptタグを作成
     let zipcode = document.getElementById('zipcode').value;  // 入力された郵便番号を取得
-    let urlText = 'https://zip-cloud.appspot.com/api/search?zipcode=' + zipcode + '&callback=callback';  // 送信するURLを作成
+    let urlText = 'https://zip-cloud.appspot.com/api/search?zipcode=' + zipcode + '&callback=zipOutput';  // 送信するURLを作成
     newScript.id = 'newScript';
     newScript.src = urlText;  // scriptのsrcにurlTextを挿入
 
@@ -15,22 +15,14 @@ push.onclick = () => {
 };
 
 // コールバック関数でJSONを受け取る
-callback = (jsonData) => {
-    console.log(jsonData);
-}
-
-// callback = (data) => {
-//     if (data.results) {
-//     // 都道府県の欄に都道府県名を表示する。
-//         let prefecture = document.getElementById('prefecture');
-//         prefecture.value = results[0].address1;
-//     // 市区町村の欄に市区町村名を表示する。
-//         let city = document.getElementById('city');
-//         city.value = address2;
-//     // 住所の欄に町域名を表示する。
-//         let address = document.getElementById('address')
-//         address.value = address3;
-//     } else {
-//         alert('該当するデータが見つかりませんでした');
-//     };
-// };
+zipOutput = (data) => {
+    let jsonData = data;
+    console.log(jsonData.results[0]);
+    if (jsonData.results[0]) {
+        document.getElementById('prefecture').value = jsonData.results[0].address1;
+        document.getElementById('city').value = jsonData.results[0].address2;
+        document.getElementById('address').value = jsonData.results[0].address3;
+    } else {
+        alert('該当するデータが見つかりませんでした');
+    };
+};
